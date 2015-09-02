a689f8e56258afd41e44fb458a68bea0e9f41432
DaniGate <daniel.garrido.terrats@gmail.com>
2015-09-02 16:11:21 +0200

    SVM and DT exercicess

--- decision_tree/dt_Dani_Gate.py
+++ decision_tree/dt_Dani_Gate.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 3 (decision tree) mini-project
 
     use an DT to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
 
 
 ### features_train and features_test are the features for the training
@@ -27,7 +28,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, features_test)
+pred = clf.predict(features_test)
 #########################################################
 
-
+acc = accuracy_score(pred, labels_test)
+print "Accuracy: %.4f" % acc
--- svm/svm_Dani_Gate.py
+++ svm/svm_Dani_Gate.py
@@ -15,6 +15,7 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -23,10 +24,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
-clf = SVC(C=1,kernel="linear",gamma=1)
+#clf = SVC(C=1,kernel="rbf",gamma=1)
+clf = SVC(kernel="rbf",C=10000.)
+
+# Shrink the size of the dataset to speed up the training:
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 t0 = time()
 clf.fit(features_train,labels_train)
@@ -37,13 +42,20 @@ t0 = time()
 pred = clf.predict(features_test)
 print "testing time:", round(time()-t0, 3), "s"
 
-prettyPicture(clf, features_test, labels_test)
-output_image("test.png", "png", open("test.png", "rb").read())
 
 
-
-from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred, labels_test)
 print "Accuracy: %.4f" % acc
+
+print "Prediction for email 10: %i" % pred[10]
+print "Prediction for email 26: %i" % pred[26]
+print "Prediction for email 50: %i" % pred[50]
+
+count = 0
+for email in pred:
+    if email == 1:
+        count += 1
+
+print "%i emails are predicted to be from Chris" % count
 
 #########################################################
